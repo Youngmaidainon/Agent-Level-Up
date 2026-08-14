a24df2e91977cf8d7c9b28cf33d903cec27a4d0d
Kittipong Wandee <137286188+Youngmaidainon@users.noreply.github.com>
2026-08-14 23:54:01 +0700

    delete test.ps1

--- cyber-security/ctf/scripts/test.ps1
+++
@@ -1,3 +0,0 @@
-Get-ChildItem -Path "D:\Up-Skill-For-Agent\cyber-security\ctf\scripts" -File |
-    Where-Object { $_.Extension -in @(".py", ".ps1", ".psm1", ".sh") } |
-    Select-Object Name, FullName
